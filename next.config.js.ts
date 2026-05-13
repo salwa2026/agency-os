0d5f0e9893b456d49af65fe7132636dce004a402
Salwa <salwa.yahya5@gmail.com>
2026-05-13 18:46:22 +0100

    Remove invalid next.config.js.ts

--- next.config.js.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['bullmq', '@prisma/client'],
-  },
-}
-
-module.exports = nextConfig
